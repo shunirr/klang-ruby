f5ac255167441771fae09add12cb36cc23d2d8c5
shunirr <m@s5r.jp>
2019-03-20 18:30:40 +0900

    Implement ン to n and ng array.

--- lib/klang/katakana.rb
+++ lib/klang/katakana.rb
@@ -12,7 +12,7 @@ module Klang
     end
 
     def to_alphabet
-      Katakana.kana2romaji(@raw)
+      convert_romaji @raw
     end
 
     private
@@ -29,25 +29,31 @@ module Klang
       end
     end
 
+    def convert_romaji(text)
+      text = Romaji.hira2kata(Romaji.normalize(text))
+      convert_n(text).flatten.map do |t|
+        Katakana.kana2romaji t
+      end
+    end
+
+    def convert_n(text)
+      unless text.include? 'ン'
+        return text
+      end
+      return [convert_n(text.sub(/ン/, 'n')), convert_n(text.sub(/ン/, 'ng'))]
+    end
+
     # From https://github.com/makimoto/romaji/blob/master/lib/romaji.rb
     def self.kana2romaji(text)
-      text = Romaji.hira2kata(Romaji.normalize(text))
       pos = 0
       k = nil
       romaji = ''
       chars = text.split(//u)
       while true
-        # ン
-        if chars[pos] == 'ン'
-          romaji += nn
-          pos += 1
-          next
-        end
-
         # ッ
         if chars[pos] == 'ッ'
           next_char_romaji = useful_romaji(Romaji::KANA2ROMAJI[chars[pos + 1]])
-          romaji += (next_char_romaji[0].slice(0,1))
+          romaji += (next_char_romaji[0].slice(0, 1))
           pos += 1
           next
         end
